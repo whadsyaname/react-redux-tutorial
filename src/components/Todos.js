1a91635c3e74674a4caba365d664f271751eec43
whads <whadsyanamexx@gmail.com>
2023-02-27 17:34:05 +0900

    init

---
+++ src/components/Todos.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const TodoItem = ({ todo, onToggle, onRemove }) => {
+	return (
+		<div>
+			<input type="checkbox" />
+			<span>example text</span>
+			<button>DEL</button>
+		</div>
+	)
+}
+
+const Todos = ({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) => {
+	const onSubmit = e => {
+		e.preventDefault();
+	}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input />
+				<button type="submit">REGIST</button>
+			</form>
+			<div>
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+			</div>
+		</div>
+	);
+};
+
+export default Todos;
